ebbf399ed90d4d6759fe272b6307fc0f3db86b93
IkaPuteriNurAzizah <ikaputerinurazizah@gmail.com>
2022-03-11 08:50:38 +0700

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './input.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
